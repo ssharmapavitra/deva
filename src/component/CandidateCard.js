26868817ba625fd20f7c45ed38574c490b9c09a6
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2022-06-26 23:46:32 +0530

    CandidateCard Update

--- src/component/CandidateCard.js
+++ src/component/CandidateCard.js
@@ -29,19 +29,20 @@ export default function CandidateCard() {
 				<div className="col-sm-3">
 					<div className="card">
 						<img
-							src="https://media-exp1.licdn.com/dms/image/C5103AQGd1Q7QeOkk2w/profile-displayphoto-shrink_400_400/0/1570086262982?e=1660780800&v=beta&t=tunvS0gMCZRMgioZ9fXQSBk6nZGytgQ-_oPLTCON7rw"
+							src="https://media-exp2.licdn.com/dms/image/C4D03AQFuF5bw_De0gw/profile-displayphoto-shrink_200_200/0/1642248891098?e=1661385600&v=beta&t=kxaYXHoCu1tqsReQ7ZXtEJfKh6qUNAmH1gQc1j4vtpg"
 							class="card-img-top"
 							alt="..."
 						/>
 						<div className="card-body">
-							<h5 className="card-title">Pavitra Sharma</h5>
+							<h5 className="card-title">Gyaneshwari Sharma</h5>
 							<p className="card-text">
-								I am an engineering student pursuing my B.Tech degree in
-								Information Technology from University Institute of Technology,
-								R.G.P.V. Bhopal.
+							A Third Year undergraduate student pursuing Bachelors 
+							in Technology(B.Tech), Information Technology, in University 
+							Institute of Technology Rajiv Gandhi Prodyougiki Vishwavidyalaya(UIT-RGPV),
+							 Bhopal 
 							</p>
 							<a
-								href="https://www.linkedin.com/in/ssharmapavitra/"
+								href="https://www.linkedin.com/in/gyaneshwari-sharma/"
 								className="btn btn-primary"
 							>
 								View More
@@ -52,19 +53,19 @@ export default function CandidateCard() {
 				<div className="col-sm-3">
 					<div className="card">
 						<img
-							src="https://media-exp1.licdn.com/dms/image/C5103AQGd1Q7QeOkk2w/profile-displayphoto-shrink_400_400/0/1570086262982?e=1660780800&v=beta&t=tunvS0gMCZRMgioZ9fXQSBk6nZGytgQ-_oPLTCON7rw"
+							src="https://media-exp2.licdn.com/dms/image/C4D03AQF-hPMBsGGReA/profile-displayphoto-shrink_200_200/0/1655884750984?e=1661385600&v=beta&t=b1GmxitVNA-Bout1119QY8bMLTjjsx-cL1zBSLL-3Ck"
 							class="card-img-top"
 							alt="..."
 						/>
 						<div className="card-body">
-							<h5 className="card-title">Pavitra Sharma</h5>
+							<h5 className="card-title">Shashwat Dixit</h5>
 							<p className="card-text">
 								I am an engineering student pursuing my B.Tech degree in
 								Information Technology from University Institute of Technology,
 								R.G.P.V. Bhopal.
 							</p>
 							<a
-								href="https://www.linkedin.com/in/ssharmapavitra/"
+								href="https://www.linkedin.com/in/shashwat-dixit-143129202/"
 								className="btn btn-primary"
 							>
 								View More
--- src/component/Result.js
+++ src/component/Result.js
@@ -3,8 +3,22 @@ import React from "react";
 export default function Result() {
 	return (
 		<div>
-			<div className="table-responsive">
-				<table className="table table-striped table-hover">
+			{/* <!-- Button trigger modal --> */}
+<button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Result
+</button>
+
+{/* <!-- Modal --> */}
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="staticBackdropLabel">These are the final Poll Results</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+	  <div classNameName="table-responsive">
+				<table classNameName="table table-striped table-hover">
 					<thead>
 						<tr>
 							<th scope="col">Candidates</th>
@@ -27,6 +41,15 @@ export default function Result() {
 					</tbody>
 				</table>
 			</div>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">New Poll</button>
+      </div>
+    </div>
+  </div>
+</div>
+			
 		</div>
 	);
 }
